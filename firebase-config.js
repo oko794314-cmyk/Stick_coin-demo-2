5fc12252d96464c5584320b5d18a070e33981a6d
MIG-IP-2019 <48225445+MIG-IP-2019@users.noreply.github.com>
2026-05-24 21:03:43 +0300

    ✅ Update Firebase config with real credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // Firebase Configuration for bb-coin-aae0a
 const firebaseConfig = {
-    apiKey: "AIzaSyD_REPLACE_WITH_YOUR_API_KEY",
+    apiKey: "AIzaSyDbfjwmvuGyUOuxNWFQuFsX6ly51fJPbbw",
     authDomain: "bb-coin-aae0a.firebaseapp.com",
-    databaseURL: "https://bb-coin-aae0a-default-rtdb.firebaseio.com",
+    databaseURL: "https://bb-coin-aae0a-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "bb-coin-aae0a",
-    storageBucket: "bb-coin-aae0a.appspot.com",
-    messagingSenderId: "REPLACE_WITH_YOUR_SENDER_ID",
-    appId: "REPLACE_WITH_YOUR_APP_ID"
+    storageBucket: "bb-coin-aae0a.firebasestorage.app",
+    messagingSenderId: "1070135810161",
+    appId: "1:1070135810161:web:66fb2be43c949d267123d0",
+    measurementId: "G-DDJTNT4HJY"
 };
 
 if (typeof firebase !== 'undefined' && firebase.apps.length === 0) {
